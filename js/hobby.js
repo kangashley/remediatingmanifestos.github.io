0e0b06fd1f71556e8ab56fa03c9e96ba45fe25e5
remediatingmanifestos <ayk267@nyu.edu>
2016-04-04 00:28:25 -0700

    add jQuery touch events
    code source: https://github.com/remediatingmanifestos/jquery-ui-for-ipad-and-iphone/issues/13
    replace Touch Punch external script

--- js/hobby.js
+++ js/hobby.js
@@ -32,4 +32,23 @@ $(document).ready(function() {
 		var $text = '<p>My Uncle Alex Vonnegut...<br>taught me something very important.<br>He said that when things are going really well<br>we should be sure to notice it.<br>He was talking about very simple occasions,<br>not great victories. <br>Maybe drinking lemonade under a shade tree,<br>or smelling the aroma of a bakery,<br>or fishing,<br>or listening to music coming from a concert hall<br>while standing in the dark outside,<br>or, dare I say, after a kiss.<br>He told me it was important at such times<br>to say out loud,<br><em>If this isn’t nice, what is?</em><br>KURT VONNEGUT</p>';
 		$('aside').html($text);
 	})
+	
+    	// Extend jQuery feature detection
+    	$.extend($.support, {
+		touch: "ontouchend" in document
+    	});
+
+    	// Hook up touch events
+	$.fn.addTouch = function() {
+        	if ($.support.touch) {
+            		this.each(function(i, el) {
+                		el.addEventListener("touchstart", iPadTouchHandler, false);
+                		el.addEventListener("touchmove", iPadTouchHandler, false);
+                		el.addEventListener("touchend", iPadTouchHandler, false);
+                		el.addEventListener("touchcancel", iPadTouchHandler, false);
+            	});
+        }
+
+        return this;
+    }
 });
