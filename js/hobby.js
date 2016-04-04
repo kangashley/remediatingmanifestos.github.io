fbf8b44ea77180ec136981b913bab6104eb66d0a
remediatingmanifestos <ayk267@nyu.edu>
2016-04-04 00:41:51 -0700

    remove touch event code

--- js/hobby.js
+++ js/hobby.js
@@ -15,26 +15,7 @@ $(document).ready(function() {
 	$('footer').hide().delay(1000).fadeIn(1000);
 		
 	/* jQUERY UI */
-	$('img.drag').draggable({stack: 'img.drag'}, function(){
-		// Extend jQuery feature detection
-    		$.extend($.support, {
-			touch: "ontouchend" in document
-    		});
-
-    		// Hook up touch events
-		$.fn.addTouch = function() {
-        		if ($.support.touch) {
-            			this.each(function(i, el) {
-                			el.addEventListener("touchstart", iPadTouchHandler, false);
-                			el.addEventListener("touchmove", iPadTouchHandler, false);
-                			el.addEventListener("touchend", iPadTouchHandler, false);
-                			el.addEventListener("touchcancel", iPadTouchHandler, false);
-            	});
-        }
-        
-        return this;
-	
-	})
+	$('img.drag').draggable({stack: 'img.drag'})
 
 	$('.about').eq(0).on('click', function() {
 		$('.about').removeClass('selected');
